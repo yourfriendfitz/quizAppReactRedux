c7da8b366f9346b821fe9f6272eeb5608c8e629d
Joshua Fitzgerald <fitzgerald913@gmail.com>
2019-09-22 20:15:04 -0500

    added imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import Login from "./components/Login";
+import requireAuth from "./components/requireAuth";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  static displayName = App.name;
+
+  render() {
+    return (
+      <Layout>
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={requireAuth(Home)} />
+      </Layout>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import styled from "styled-components";
+import * as Palette from "./components/Palette";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import authReducer from "./store/auth";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({
+  authRed: authReducer
+});
+
+const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+const rootElement = document.getElementById("root");
+
+const Container = styled.div`
+  background-image: linear-gradient(
+    to right bottom,
+    ${Palette.Primary},
+    ${Palette.AltPrimary}
+  );
+  display: grid;
+`;
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <BrowserRouter basename={baseUrl}>
+    <Container className="overflow-auto p-0 m-0 vh-100">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Container>
+  </BrowserRouter>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
